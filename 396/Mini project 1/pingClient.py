67ded624405644c6b764785ac28c4c60da219503
Jackson Darling <audarling99@gmail.com>
2022-02-14 09:35:25 -0800

    still pinging

--- 396/Mini project 1/pingClient.py
+++ 396/Mini project 1/pingClient.py
@@ -2,25 +2,25 @@
 from socket import *
 import time
 serverName = '127.0.0.1'
-serverPort = 12000
+serverPort = 6790
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 pingy='hewwo?'
-clientSocket.connect((serverName,serverPort))
+doney="done!"
 while True:
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    clientSocket.connect((serverName,serverPort))
-    for i in range(1,10):
+    for i in range(1,11):
         time.sleep(1)
         now = time.time()
         clientSocket.sendto(pingy.encode(), (serverName, serverPort))
         Message, serverAddress = clientSocket.recvfrom(2048)
         if Message.decode() == 'ERROR':
-            print('*')
+            print(str(i) + '*')
         else:
             recievedAt = time.time()
             totalTime = recievedAt-now
             line = str(i) +'. ' + str(Message) + ' RTT ' + str(totalTime) + 's'
             print(line)
+    clientSocket.sendto(doney.encode(), (serverName, serverPort))
+    print("done!")
     clientSocket.close()
 
 
