edd3058aa2e8a17386846baef717ff2cf8cf10bc
Jackson Darling <audarling99@gmail.com>
2022-02-16 10:53:36 -0800

    Ping Finally Works
    Only on mac lol

--- 396/Mini project 1/pingClient.py
+++ 396/Mini project 1/pingClient.py
@@ -6,9 +6,10 @@ serverPort = 6790
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 pingy='hewwo?'
 doney='done!'
+clientSocket.settimeout(5)
 while True:
-    try: 
-        for i in range(0,10):
+    for i in range(0,10):
+        try: 
             now = time.time()
             clientSocket.sendto(pingy.encode(), (serverName, serverPort))
             Message, serverAddress = clientSocket.recvfrom(2048)
@@ -17,13 +18,14 @@ while True:
             rttMs = totalTime*1000
             line = str(i) +'. ' + str(Message) + ' RTT ' + str(rttMs) + 'ms'            
             print(line)
-            # if u DONT recieve a packet: print(str(i) + '*')
-    except clientSocket.Timeouterror:
-        print(str(i) + '*')
-
-    clientSocket.sendto(doney.encode(), (serverName, serverPort))
-    print("done!")
-    clientSocket.close()
+        except TimeoutError:
+            print(str(i) +'. ' + '*')
+            continue
+        # if u DONT recieve a packet: print(str(i) + '*')
+    break
+clientSocket.sendto(doney.encode(), (serverName, serverPort))
+print("done!")
+clientSocket.close()
 
 
 
--- 396/Mini project 1/pingServer.py
+++ 396/Mini project 1/pingServer.py
@@ -3,26 +3,33 @@
 import random
 # Import socket module
 from socket import *
-probab = 0.1
+import time
+probab = 0.9
 # SOCK_STREAM for TCP, SOCK_DGRAM for UDP
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
 serverSocket.bind(('127.0.0.1', 6790))
 print ("Server is ready to receive")
 while True:
-    # Receive client packet and arrival address
-    message, address = serverSocket.recvfrom(2048)
-    # Capitalize the message
-    message = message.upper()
-    # Error simulator goes here
-    errored = random.random()
-    if errored <= probab:
-        pass
-    # Your code ends here
-    else:
-        serverSocket.sendto(message, address)
-        print(";)")
-
-    if message == 'done!':
-        print("Au revoir!")
-        serverSocket.close()
+    try: 
+        errored = random.random()
+        if errored <= probab:
+            print("packet dropped lol")
+            time.sleep(5)
+            continue
+        # Receive client packet and arrival address
+        message, address = serverSocket.recvfrom(2048)
+        # Capitalize the message
+        message = message.upper()
+        # Error simulator goes here
+        if message == 'done!':
+            print("Au revoir!")
+            serverSocket.close()
+        else:
+            serverSocket.sendto(message, address)
+            print("succesfful")
+            continue
+    except TimeoutError:
+        continue
+    break
+serverSocket.close()
