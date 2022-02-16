7f79fbe197f560d923ce4a6ed848a05eb9c71d29
Jackson Darling <audarling99@gmail.com>
2022-02-16 13:28:40 -0800

    Added comments to Mini Project 1: Finished

--- 396/Mini project 1/pingClient.py
+++ 396/Mini project 1/pingClient.py
@@ -6,25 +6,40 @@ serverPort = 6790
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 pingy='hewwo?'
 doney='done!'
+#Set Timeout for error simulator
 clientSocket.settimeout(5)
 while True:
+    #10 pings...
     for i in range(0,10):
         try: 
+            #log the time
             now = time.time()
+            #send message
             clientSocket.sendto(pingy.encode(), (serverName, serverPort))
+            #recieve message
             Message, serverAddress = clientSocket.recvfrom(2048)
+            #log the time 
             recievedAt = time.time()
+            #Total time in seconds (recieved-sent)
             totalTime = recievedAt-now
+            #Convert to Ms
             rttMs = totalTime*1000
-            line = str(i) +'. ' + str(Message) + ' RTT ' + str(rttMs) + 'ms'            
+            #Construct line to print to console
+            line = str(i) +'. ' + str(Message) + ' RTT ' + str(rttMs) + 'ms' 
+            #Print it!           
             print(line)
+            #If there is a "timeout" error...
         except TimeoutError:
+            #Print star + line
             print(str(i) +'. ' + '*')
+            #Continue with loop
             continue
-        # if u DONT recieve a packet: print(str(i) + '*')
+    #After for loop, break While loop
     break
+#Send goodbye message (does not work)
 clientSocket.sendto(doney.encode(), (serverName, serverPort))
 print("done!")
+#Close socket
 clientSocket.close()
 
 
--- 396/Mini project 1/webclient.py
+++ 396/Mini project 1/webclient.py
@@ -11,16 +11,19 @@ obj = input("Object: ")
 host = ''
 port = 6010
 obj= 'jim.html'
+#Create TCP socket
 clientSocket = socket(AF_INET,SOCK_STREAM)
 try:
+    #Connect to server socket
     clientSocket.connect((host,port))
     print("connected")
+    #Send HTTP request
     request= 'GET /'+obj+' HTTP/1.1'+"\r\n"
     clientSocket.send(request.encode())
     clientSocket.send("\r\n".encode())
     clientSocket.send("\r\n".encode())
     print("request sent")
-
+#Recieve header (inputData) and body of requested object (responseData) and print them
     inputData = clientSocket.recv(2048)
     responseData= clientSocket.recv(2048)
     print("header: ")
@@ -31,6 +34,7 @@ try:
     print("response recieved; goodbye")
 
     clientSocket.close()
+    #Error exception
 except IOError:
     print("You're mom.")
     clientSocket.close()
