286790e8c23443e69c0bd62e3cc5cd268bd10576
Jackson Darling <audarling99@gmail.com>
2022-04-05 11:24:44 -0700

    Update huffman.cc

--- 221/Hw8/JDNA/huffman.cc
+++ 221/Hw8/JDNA/huffman.cc
@@ -2,15 +2,22 @@
 #include"huffman.hh"
 
 
-// Encode a symbol into a sequence of bits, then update frequency table.
-  Huffman::bits_t 
-  encode(int symbol){
-      
-
+//Build Forest method
+  HForest
+  build_forest(std::vector<int> freqs){
+    HForest forest;
+    for (int i=0; i < freqs.size(); i++){
+      forest.add_tree(HTree::tree_ptr_t(new HTree(i,freqs[i])));
+      };
+    return forest;
   };
 
 
-
+// Encode a symbol into a sequence of bits, then update frequency table.
+  Huffman::bits_t 
+  encode(int symbol){
+    
+  };
 
 
   // Decode a single bit into a symbol. If no symbol can be unmabiguously decoded
