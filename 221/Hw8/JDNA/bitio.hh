1ab5077ba09d6cbd56c70aaf6b7957c8b022ce73
Jackson Darling <audarling99@gmail.com>
2022-04-08 17:14:05 -0700

    Finished* Bitio

--- 221/Hw8/JDNA/bitio.hh
+++ 221/Hw8/JDNA/bitio.hh
@@ -14,19 +14,20 @@ class BitInput {
   BitInput(std::istream& is)
    : is_(is), buff_(0), count_(0) {}
 
-  BitInput(const BitInput&) = default;
-  BitInput(BitInput&&) = default;
-  BitInput& operator=(const BitInput&) = default;
-  BitInput& operator=(BitInput&&) = default;
+  BitInput(const BitInput&) = delete;
+  BitInput(BitInput&&) = delete;
+  BitInput& operator=(const BitInput&) = delete;
+  BitInput& operator=(BitInput&&) = delete;
 
   // Read a single bit (or trailing zero)
   // Allowed to crash or throw an exception if called past end-of-file.
   bool input_bit();
   
   private:
+    std::istream& is_;
     uint8_t buff_;
     uint8_t count_;
-    std::istream& is_;
+    
 };
 
 // BitOutput: Write a single bit at a time to an output stream
@@ -40,16 +41,17 @@ class BitOutput {
   // Flushes out any remaining output bits and trailing zeros, if any:
   ~BitOutput();
 
-  BitOutput(const BitOutput&) = default;
-  BitOutput(BitOutput&&) = default;
-  BitOutput& operator=(const BitOutput&) = default;
-  BitOutput& operator=(BitOutput&&) = default;
+  BitOutput(const BitOutput&) = delete;
+  BitOutput(BitOutput&&) = delete;
+  BitOutput& operator=(const BitOutput&) = delete;
+  BitOutput& operator=(BitOutput&&) = delete;
 
   // Output a single bit (buffered)
   void output_bit(bool bit);
   private:
+    std::ostream& os_;
     uint8_t buff_;
     uint8_t count_;
-    std::ostream& os_;
+    
 };
 
